52b143a7b8b5adeeea04f80df0849a1d829f75d7
Nikolay Nanov <nickolaynanov17@gmail.com>
2023-01-14 20:45:30 +0200

    initial commit

---
+++ Async-Binary-Search/Async-Binary-Search/BinarySearchTree.cs
@@ -0,0 +1,86 @@
+﻿namespace Async_Binary_Search
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(int value) => Value = value;
+    }
+
+    public class BinarySearchTree
+    {
+        private Node? root;
+
+        // Inserts a new value into the tree
+        public async Task InsertAsync(int value)
+        {
+            // If tree is empty, new value becomes the root
+            if (root == null)
+            {
+                root = new Node(value);
+                return;
+            }
+
+            // Start at the root
+            Node? current = root;
+            while (current != null)
+            {
+                // If new value is less than current value, move to left child
+                // If left child is null, insert new value as left child
+                // If new value is greater than or equal to current value, move to right child
+                if (value < current.Value)
+                {    
+                    if (current.Left == null)
+                    {
+                        current.Left = new Node(value);
+                        return;
+                    }
+                    current = current.Left;
+                }
+                else
+                {
+                    // If right child is null, insert new value as right child
+                    if (current.Right == null)
+                    {
+                        current.Right = new Node(value);
+                        return;
+                    }
+                    current = current.Right;
+                }
+                await Task.Delay(10);  //simulating some asynchronous processing 
+            }
+        }
+
+        public async Task<bool> FindAsync(int value)
+        {
+            // Start at the root
+            Node? current = root;
+
+            while (current != null)
+            {
+                // If current value is equal to the target, return true
+                // If target value is less than current value, move to left child
+                // If target value is less than current value, move to left child
+                // If target value is greater than current value, move to right child
+                if (value == current.Value)
+                {
+                    return true;
+                }
+                else if (value < current.Value)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+                await Task.Delay(10);  //simulating some asynchronous processing 
+            }
+
+            // Target not found, return false
+            return false;
+        }
+    }
+}
---
+++ Async-Binary-Search/Async-Binary-Search/Program.cs
@@ -0,0 +1,27 @@
+﻿using Async_Binary_Search;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        BinarySearchTree bst = new();
+
+        await bst.InsertAsync(5);
+        await bst.InsertAsync(3);
+        await bst.InsertAsync(7);
+        await bst.InsertAsync(1);
+        await bst.InsertAsync(4);
+        await bst.InsertAsync(9);
+
+        bool result = await bst.FindAsync(7);
+
+        if (result)
+        {
+            Console.WriteLine("7 found in the tree");
+        }
+        else
+        {
+            Console.WriteLine("7 not found in the tree");
+        }
+    }
+}
